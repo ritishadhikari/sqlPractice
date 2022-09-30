14c629ee64d4978f1dbc7e048107835d0bbb8a19
Ritish Adhikari <ritishadhikari@gmail.com>
2022-09-30 16:03:17 +0530

    Till 09/30/2022 16:03:17

---
+++ StoredProcedure.sql
@@ -0,0 +1,18 @@
+DELIMITER $$
+/*
+CREATE OR REPLACE PROCEDURE
+	pr_name
+		(
+			p_name VARCHAR,
+            p_age INT
+        )
+	AS
+	DECLARE
+		variable data type;
+	BEGIN
+		PROCEDURE body - all logics
+	END $$
+    
+*/
+
+-- Procedure Without Parameters
