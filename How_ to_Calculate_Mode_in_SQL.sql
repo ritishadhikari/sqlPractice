71f041ec8d1f9391dba77a31ea262a2c664de74e
Ritish Adhikari <ritishadhikari@gmail.com>
2022-10-14 17:03:19 +0530

    Till 10/14/2022 17:03:19

---
+++ How_ to_Calculate_Mode_in_SQL.sql
@@ -0,0 +1,29 @@
+USE org;
+
+CREATE TABLE mode
+	(
+		id INT
+    );
+    
+INSERT INTO mode
+	(id)
+    VALUES
+    (1),
+    (2),
+    (2),
+    (2),
+    (2),
+    (3),
+    (3),
+    (3),
+    (3),
+    (4),
+    (5);
+    
+SELECT 
+	*
+FROM 
+	mode;
+
+
+DELETE FROM mode;
